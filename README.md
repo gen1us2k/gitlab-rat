cdb1ee65394698cf1a053d4e18b09023ed7974d6
Gen1us2k <minkin.andrew@gmail.com>
2017-09-06 19:52:17 +0600

    Re-fuck-tored

---
+++ README.md
@@ -0,0 +1,1 @@
+python gitlab-to-trello.py -u gitlabusernae -t trello_card_id --gitlab_token=your_private_token --trello_key=trello_key --trello_token=trello_token
--- gitlab-to-trello.py
+++ gitlab-to-trello.py
@@ -1,62 +1,72 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import sys
 import requests
-import datetime
+
+from datetime import datetime, timedelta
+from optparse import OptionParser
 
 
-def info():
-    user_name = input('Введите имя пользователя на gitlab.com: ')  # Имя Пользователя на gitlab.com
-    get_user_id = 'https://gitlab.com/api/v4/users?username={0}'.format(user_name)
-    r = requests.get(get_user_id)
-    user_id = r.json()[0]['id']
+DAY_OFFS = (0, 6)
 
-    gitlab_url = 'https://gitlab.com/api/v4/users/{0}/events'.format(user_id)  # URL на события
-    r = requests.get(gitlab_url).json()
 
-    date_list = []
-    for current_date in r:
-        current_date = current_date['created_at']
-        date = current_date.rsplit('T', 1)[0]
-        date_list.append(date)
+def get_commits_status_for_user(username, token):
+    print("Grabbing user info for {} username".format(username))
+    resp = requests.get("https://gitlab.com/api/v4/users?username={}".format(username))
+    user_id = resp.json()[0].get('id', 0)
+    if user_id == 0:
+        return "user {} not found".format(username)
 
-    now_date = datetime.date.today()
-    start = now_date - datetime.timedelta(days=1)
-    delta = datetime.timedelta(days=7)
-    end = start - delta
-
-    week_list = []
-    while end < start:
-        end = end + datetime.timedelta(days=1)
-        date = end
-        date = str(date)
-        week_list.append(date)
-
-    daily_commits = []
-    for current_date in week_list:
-        commit = date_list.count(current_date)
-        if commit:
-            info = ['за', current_date, 'коммитов было-', commit]
-            daily_commits.append(info)
-        else:
-            info = ['за', current_date, 'коммитов не было']
-            daily_commits.append(info)
-    return daily_commits
-
-info = info()
-
-def send(info):
-    id_card = input('Введите id карточки на trello.com: ')  # ID Нашей карточки в trello.com. Пример 59ae5dfc529cc2277f6a75bd или la8gztsN
-    url = ("https://api.trello.com/1/cards/{0}/actions/comments").format(id_card)  # URL на добавления комментария в карточку
-
-    text = info
-    key = '433edb45fe4cb614c1124eee5733156a'  # Наш ключ в trello.com
-    token = '2f01994a775cb341ff07d7f4901d5c445b2e686a2da3ec086efaf52b8f8d9ee4'  # Наш токен в trello.com
-
-    querystring = {
-        'text': text,
-        'key': key,
-        'token': token
+    print("Grabbing events data...")
+    params = {
+        "private_token": token,
+        "before": datetime.now().strftime("%Y-%m-%d"),
+        "after": (datetime.now() - timedelta(days=7)).strftime("%Y-%m-%d"),
     }
+    r = requests.get("https://gitlab.com/api/v4/users/{}/events".format(user_id), params=params)
+    messages = []
+    for i in xrange(7):
+        day = datetime.now() - timedelta(i)
+        commits = 0
+        for event in r.json():
+            commit_date = datetime.strptime(event.get("created_at"), "%Y-%m-%dT%H:%M:%S.%fZ")
+            if (day - commit_date).days == 0:
+                commits += 1
 
-    response = requests.request("POST", url, params=querystring)  # Запрос на добавления комментария в карточку
-    return response
+        if (datetime.now() - day).days == 0 and commits == 0:
+            continue
 
-send(info)
+        if commits == 0 and day.weekday() not in DAY_OFFS:
+            messages.append("No commits for {}".format(day.strftime("%Y-%m-%d")))
+
+        if commits > 0 and day.weekday() in DAY_OFFS:
+            messages.append("There are {} commits on day off of {}".format(commits, day.strftime("%Y-%m-%d")))
+
+    return "\n".join(messages)
+
+
+def send_to_trello(key, token, card_id, message):
+    data = {
+        "text": message,
+        "key": key,
+        "token": token,
+    }
+    r = requests.post("https://api.trello.com/1/cards/{}/actions/comments".format(card_id), data=data)
+    return r.content
+
+
+if __name__ == '__main__':
+    parser = OptionParser()
+    parser.add_option("-u", "--username", dest="username", help="Gitlab username for user to check to")
+    parser.add_option("-t", "--trello-card-id", dest="trello_id", help="Trello card ID to leave comment")
+    parser.add_option("--gitlab_token", dest="gitlab_token", help="Gitlab private token")
+    parser.add_option("--trello_key", dest="trello_key", help="Gitlab private token")
+    parser.add_option("--trello_token", dest="trello_token", help="Gitlab private token")
+
+    (options, args) = parser.parse_args()
+    if not options.username or not options.trello_id or not options.gitlab_token:
+        parser.print_help()
+        sys.exit(0)
+
+    status_message = get_commits_status_for_user(options.username, options.gitlab_token)
+    print send_to_trello(options.trello_key, options.trello_token, options.trello_id, status_message)
